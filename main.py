741e0d5815e905a614ebd6ba2bb41b9f77e72d08
Darthmazgar <34077975+Darthmazgar@users.noreply.github.com>
2019-02-28 18:19:42 +0000

    output

--- main.py
+++ main.py
@@ -9,14 +9,29 @@ def readfile(name):
     for i, line in enumerate(file):
         data.append(line)
         # data_arr[i] = line
-        print(line)
+        # print(line)
     # print(data)
     return data
+
+def alt_input():
+    n = int(input(""))
+
+    for i in range(n):
+        pass
+
+def output(slideshow):
+    print(len(slideshow))
+    for i in range(1, len(slideshow)):
+        print(slideshow[i])
+    return 0
+
 
 def main():
     name = 'a_example.txt'
     data = readfile(name)
-    print(data)
+    # print(data)
+    sample_slides = ['3', '1 2', '3', '0']
+    output(sample_slides)
 
 
 if __name__ == '__main__':
