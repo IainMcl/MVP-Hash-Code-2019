27dd6910fb7a71af43dd73bee607a67f0c9978b0
Darthmazgar <34077975+Darthmazgar@users.noreply.github.com>
2019-02-28 18:09:41 +0000

    read file

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import numpy as np
 
-main():
-    pass
+def readfile(name):
+    file = open(name, 'r')
+    length = int(file.readline())
+    # print(length)
+    # data_arr = np.zeros(length, dtype=np.unicode_)
+    data = []
+    for i, line in enumerate(file):
+        data.append(line)
+        # data_arr[i] = line
+        print(line)
+    # print(data)
+    return data
+
+def main():
+    name = 'a_example.txt'
+    data = readfile(name)
+    print(data)
 
 
 if __name__ == '__main__':
